bc10da4e5d1b5dd7276a10f512dabc6472364c1f
ggriselda <vanphuocskttn123@gmail.com>
2023-05-09 22:13:16 +0700

    first commit

--- assets/js/project.js
+++ assets/js/project.js
@@ -9,9 +9,9 @@ const projects = [
     title: "Quiz App",
     cardImage: "assets/images/project-page/quiz.jpg",
     description: "A quiz app built using basic react.",
-    tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
     Previewlink: "",
     Githublink: "",
+  
   },
   {
     title: "Chess Engine",
